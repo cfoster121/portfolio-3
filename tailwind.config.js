61f1366f846040af84392929d9c131b6eb507e16
Courtney Foster <cfoster649@gmail.com>
2022-02-19 00:32:25 -0800

    Format config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,css}", "./dist/**/*.{html,js,css}", "./**/*..{html}", ],
+  content: ["./src/**/*.{html,js,css}", "./dist/**/*.{html,js,css}", "index.html" ],
   theme: {
     borderWidth: {
       DEFAULT: '1px',
